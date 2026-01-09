57520274d3938bedddf62d6bae5664df1a8f536a
Sreeparna Bhowmick <sreeparnabhowmick8@gmail.com>
2026-01-09 23:41:32 +0530

    Time: 4 ms (42.70%) | Memory: 19.6 MB (10.32%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.py
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.py
@@ -1,25 +1,15 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
-        # nums.sort()
-        # l=0
-        # r=len(nums)-1
-        # count=0
-        # while(l<r):
-        #     sum1=nums[l]+nums[r]
-        #     if(sum1<target):
-        #         count+=1
-        #         r-=1
-        #     elif(sum1==target):
-        #         r-=1
-        #     else:
-        #         l+=1
-        # return count   
-        count=0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                sum1=nums[i]+nums[j]
-                if(sum1<target):
-                    count+=1
+        nums.sort() # sort the vector nums
+        count = 0 # variable to store the count
+        left = 0 # variable to store the left
+        right = len(nums)-1 # variable to store the right
+        while(left < right): # loop until left is less than right
+            if(nums[left] + nums[right] < target): # if nums[left] + nums[right] is less than target
+                count += right-left # update the count
+                left += 1 # increment the left
+            else: # else
+                right -= 1 # decrement the right
         return count               
 
         
