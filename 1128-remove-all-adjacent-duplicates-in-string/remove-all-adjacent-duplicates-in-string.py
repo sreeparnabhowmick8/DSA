4b68943ef60072591bf3b5dd386a0055bfe689cd
Sreeparna Bhowmick <sreeparnabhowmick8@gmail.com>
2026-02-15 20:33:45 +0530

    Time: 16 ms (88.82%) | Memory: 20.1 MB (47.83%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack=[]
+        for i in s:
+            if stack:
+                if(i==stack[-1]):
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+        return "".join(stack)                    
+        
