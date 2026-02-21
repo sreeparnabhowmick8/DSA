cc8d38def11d391b89449921242a1b4455ba5b11
Sreeparna Bhowmick <sreeparnabhowmick8@gmail.com>
2026-02-21 20:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (24.76%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,15 +1,22 @@
 class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        dic={}
+    def nextGreaterElement(self, num1: List[int], num2: List[int]) -> List[int]:
         stack=[]
-        for i in nums2:
-            while stack and stack[-1]<i:
-                dic[stack.pop()]=i
-            stack.append(i)
-        while stack:
-            dic[stack.pop()]=-1
-        return [dic[i] for i in nums1]            
-
-
-
+        ans=[]
+        res=[]
+        for i in range(len(num2)-1,-1,-1):
+            while(len(stack)>0 and num2[i]>stack[-1]):
+                stack.pop()
+            if(len(stack)==0):
+                ans.append(-1) 
+                stack.append(num2[i])
+            else:
+                ans.append(stack[-1])
+                stack.append(num2[i]) 
+        ans1=ans[::-1] 
+        dic={}
+        for i in range(len(num2)):
+            dic[num2[i]]=ans1[i]
+        for i in num1:
+            res.append(dic[i])
+        return res
         
