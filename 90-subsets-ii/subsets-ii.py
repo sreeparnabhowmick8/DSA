d3fe9706ca41dceb6b65d9028edc6a833f27dd41
Sreeparna Bhowmick <sreeparnabhowmick8@gmail.com>
2026-03-21 00:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (51.09%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  # step 1: sort to group duplicates
+        res = []
+
+        def backtrack(start, path):
+            res.append(path[:])  # add current subset
+        
+            for i in range(start, len(nums)):
+            # step 2: skip duplicates
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+            
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
+        
