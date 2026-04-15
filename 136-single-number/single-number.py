5d9d87be714773f6fcb20dd2840805ff0f8dde11
Sreeparna Bhowmick <sreeparnabhowmick8@gmail.com>
2026-04-15 20:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (77.11%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,8 @@
 class Solution:
     def singleNumber(self, arr: List[int]) -> int:
-        dic={}
+        res=0
         for i in arr:
-            dic[i]=dic.get(i,0)+1
-        for i in arr:
-            if(dic[i]==1):
-                return i           
+            res^=i
+        return res               
             
         
