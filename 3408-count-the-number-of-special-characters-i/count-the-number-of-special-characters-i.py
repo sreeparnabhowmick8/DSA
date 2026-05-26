a9b9bf3e1e0a9e4b3e2e168a2ebe600fc99c2de3
Sreeparna Bhowmick <sreeparnabhowmick8@gmail.com>
2026-05-26 15:17:12 +0530

    Time: 2 ms (42.11%) | Memory: 19.3 MB (64.16%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -5,12 +5,12 @@ class Solution:
         count=0
         for i in word:
             if i>='a' and i<='z':
-                small[ord(i)-ord('a')]=1 #small=[1,1,1,...]
-            else:
-                capital[ord(i)-ord('A')]=1 #capital=[1,1,1,...]
+                small[ord(i)-ord('a')]=1
+            if i>='A' and i<='Z':
+                capital[ord(i)-ord('A')]=1
         for i in range(26):
-            if(small[i]==1 and capital[i]==1):
-                count+=1 
-        return count                   
+            if(small[i])==1 and capital[i]==1:
+                count+=1
+        return count                                       
 
         
